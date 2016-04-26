b56804aa45b05f22d372de8a84dbb492bde04aa0
demofan <demo@demo.tc>
2016-04-26 19:24:59 +0800

    資料列表顯示（尚無分頁）

--- SortSearchAndPagedSample/SortSearchAndPagedSample/Controllers/HomeController.cs
+++ SortSearchAndPagedSample/SortSearchAndPagedSample/Controllers/HomeController.cs
@@ -3,13 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using SortSearchAndPagedSample.Models;
 
 namespace SortSearchAndPagedSample.Controllers
 {
     public class HomeController : Controller
     {
+        private Model1 _dbContext;
+
+        public HomeController()
+        {
+            _dbContext = new Model1();
+        }
+
         public ActionResult Index()
         {
+            ViewData.Model = _dbContext.AccountBook.ToList();
             return View();
         }
 
