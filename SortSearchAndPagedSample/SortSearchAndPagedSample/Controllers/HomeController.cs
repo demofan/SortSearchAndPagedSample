38e033035a69530ad9ab727e8e6fd780827230af
demofan <demo@demo.tc>
2016-04-26 19:48:26 +0800

    完成排序、分頁、搜尋功能

--- SortSearchAndPagedSample/SortSearchAndPagedSample/Controllers/HomeController.cs
+++ SortSearchAndPagedSample/SortSearchAndPagedSample/Controllers/HomeController.cs
@@ -26,17 +26,26 @@ namespace SortSearchAndPagedSample.Controllers
         /// <param name="column">搜尋欄位名稱 預設為 Dateee</param>
         /// <param name="order">排序方向 預設為 Ascending</param>
         /// <returns></returns>
-        public ActionResult Index(int? page, string q)
+        public ActionResult Index(int? page, string q, string column = "Dateee", EnumSort order = EnumSort.Ascending)
         {
             //分頁套件： Install-Package PagedList.Mvc 
             var pageIndex = page.HasValue ? page.Value < 1 ? 1 : page.Value : 1;
-            var pageSize = 5;
+            var pageSize = 2;
 
             //為了範例最簡化，因此直接在 Controller 操作 DB ，實務上請盡量避免
             var source = _dbContext.AccountBook.AsQueryable();
-          
+
+            if (string.IsNullOrWhiteSpace(q) == false)
+            {
+                // 只是單純示範搜尋條件應該如何累加
+                var category = Convert.ToInt32(q);
+                source = source.Where(d => d.Categoryyy == category);
+            }
+
             var result = new QueryOption<AccountBook>
             {
+                Order = order,
+                Column = column,
                 Page = pageIndex,
                 PageSize = pageSize,
                 Keyword = q
